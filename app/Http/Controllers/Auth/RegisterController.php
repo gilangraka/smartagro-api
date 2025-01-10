2f7e1b3cc81930f63b28b8148ec9ca18176b8d2e
Maulidaa <maulidaallya06@gmail.com>
2025-01-10 21:11:47 +0700

    fix: update registration

--- app/Http/Controllers/Auth/RegisterController.php
+++ app/Http/Controllers/Auth/RegisterController.php
@@ -19,13 +19,13 @@ class RegisterController extends BaseController
     {
         try {
             $validatedData = $request->validate([
-                'name' => 'required|string|max:255',
+                'username' => 'required|string|max:255',
                 'email' => 'required|string|email|max:255|unique:users',
                 'password' => 'required|string|min:8|confirmed',
             ]);
 
             $user = User::create([
-                'name' => $validatedData['name'],
+                'username' => $validatedData['name'],
                 'email' => $validatedData['email'],
                 'password' => Hash::make($validatedData['password']),
             ]);
