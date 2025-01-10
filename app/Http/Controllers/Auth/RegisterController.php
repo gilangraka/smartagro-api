403da6f77f97644465c5a30d7678226f7d080964
Maulidaa <maulidaallya06@gmail.com>
2025-01-10 22:50:29 +0700

    feat: update registration logic

--- app/Http/Controllers/Auth/RegisterController.php
+++ app/Http/Controllers/Auth/RegisterController.php
@@ -9,6 +9,9 @@ use App\Http\Controllers\BaseController;
 use Illuminate\Support\Facades\Hash;
 use Illuminate\Support\Facades\Log;
 use Illuminate\Http\Request;
+use Illuminate\Validation\ValidationException;
+use Exception;
+use Symfony\Component\HttpKernel\Exception\BadRequestHttpException;
 
 class RegisterController extends BaseController
 {
@@ -19,24 +22,34 @@ class RegisterController extends BaseController
     {
         try {
             $validatedData = $request->validate([
-                'username' => 'required|string|max:255',
+                'name' => 'required|string|max:255',
                 'email' => 'required|string|email|max:255|unique:users',
                 'password' => 'required|string|min:8|confirmed',
             ]);
 
+            Log::info('RegisterController: ' . $validatedData['name'] . ' is trying to register.');
+
             $user = User::create([
-                'username' => $validatedData['name'],
+                'name' => $validatedData['name'],
                 'email' => $validatedData['email'],
                 'password' => Hash::make($validatedData['password']),
             ]);
 
             $token = $user->createToken('authToken')->plainTextToken;
 
+            
+
             return $this->sendResponse(['user' => $user, 'token' => $token], 'User registered successfully.');
-        } catch (\Illuminate\Validation\ValidationException $e) {
+        } catch (BadRequestHttpException $e) {
+            Log::error('RegisterController: ' . $e->getMessage());
+            return $this->sendError($e->getMessage(), 400);
+        } catch (ValidationException $e) {
+            Log::error('RegisterController: ' . $e->getMessage());
             return $this->sendError($e->errors(), 422);
-        } catch (\Exception $e) {
+        } catch (Exception $e) {
+            Log::error('RegisterController: ' . $e->getMessage());
             return $this->sendError('An unexpected error occurred.', 500);
-        }
+        } 
+
     }
 }
